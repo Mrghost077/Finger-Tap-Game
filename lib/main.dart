2110be5a9be58c0d9a62b54ce97569fd18095bc9
mrghost077 <msnlms86@gmail.com>
2025-02-11 21:39:26 +0530

    finished creating mainpage ui

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,36 @@ class MainPage extends StatelessWidget{
             color: Colors.blueAccent,
             height: MediaQuery.of(context).size.height/2,
             width: double.infinity,
+            child: Center(
+              child: MaterialButton(
+                onPressed: (){
+              
+                },
+                child: Text("START"),
+                color: Colors.white,
+                shape: CircleBorder(),
+                height: 100,
+                minWidth: 100,
+                ),
+            ),
           ),
 
           Container(
             color: Colors.redAccent,
             height: MediaQuery.of(context).size.height/2,
             width: double.infinity,
+            child: Center(
+              child: MaterialButton(
+                onPressed: (){
+              
+                },
+                child: Text("START"),
+                color: Colors.white,
+                shape: CircleBorder(),
+                height: 100,
+                minWidth: 100,
+                ),
+            ),
           )
         ],
       ),
