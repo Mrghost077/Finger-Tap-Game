5e3e863fc75d943e3bd67820ce76b36ad0d5a343
mrghost077 <msnlms86@gmail.com>
2025-02-12 20:58:23 +0530

    Succesfully created game page

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MainPage extends StatelessWidget{
             child: Center(
               child: MaterialButton(
                 onPressed: (){
-              
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => GamePage()));
                 },
                 child: Text("START"),
                 color: Colors.white,
@@ -37,7 +37,7 @@ class MainPage extends StatelessWidget{
             child: Center(
               child: MaterialButton(
                 onPressed: (){
-              
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => GamePage()));
                 },
                 child: Text("START"),
                 color: Colors.white,
@@ -50,5 +50,139 @@ class MainPage extends StatelessWidget{
         ],
       ),
     );
+  }
+}
+
+class GamePage extends StatefulWidget{
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+
+double redCardHeight = 0;
+double blueCardHeight = 0;
+int redCardScore = 0;
+int blueCardScore = 0;
+double winningHeight = 0;
+
+bool initialize = false;
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    winningHeight = MediaQuery.of(context).size.height - 60;
+
+    if (initialize == false){
+      blueCardHeight = MediaQuery.of(context).size.height/2;
+      redCardHeight = MediaQuery.of(context).size.height/2;
+
+      initialize = true;
+    }
+
+    return Scaffold(
+      body: Column(
+        children: [
+          MaterialButton(
+            padding: EdgeInsets.zero,
+
+            onPressed: (){
+              setState(() {
+                blueCardHeight = blueCardHeight + 30;
+                redCardHeight = redCardHeight - 30;
+                blueCardScore = blueCardScore + 5;
+              });
+
+              if (blueCardHeight > winningHeight){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> ResultPage()));
+            }
+            },
+
+            
+
+            child: Container(
+              color: Colors.blueAccent,
+              height: blueCardHeight,
+              width: double.infinity,
+               alignment: Alignment.topLeft,
+              padding: EdgeInsets.all(10),
+              
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text("Player B ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                  ),
+                  Text(blueCardScore.toString(),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                ],
+              ),
+            ),
+          ),
+
+          MaterialButton(
+
+            padding: EdgeInsets.zero,
+
+            onPressed: (){
+              setState(() {
+                redCardHeight = redCardHeight + 30;
+                blueCardHeight = blueCardHeight - 30;
+                redCardScore = redCardScore + 5;
+              });
+
+              if (redCardHeight > winningHeight){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> ResultPage()));
+            }
+            },
+
+            
+
+            child: Container(
+              color: Colors.redAccent,
+              height: redCardHeight,
+              width: double.infinity,
+              alignment: Alignment.bottomLeft,
+              padding: EdgeInsets.all(10),
+              
+              child: Row(
+                
+                children: [
+                  Expanded(
+                    child: Text("Player A ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                  ),
+                  Text(redCardScore.toString(),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ResultPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
